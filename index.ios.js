7c02711fc58c75ebf56e3fe87542a6514adfcb16
Potchara Pheungthonglor <potcharajoe@gmail.com>
2017-04-19 08:45:09 +0700

    now can show map and use axios

--- index.ios.js
+++ index.ios.js
@@ -9,27 +9,100 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 import MapView from 'react-native-maps';
+import axios from 'axios';
 
 export default class Drone_visualization extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showText: true,
+      annotate: 'My Locaition :D',
+      region: {
+        latitude: 13.8463,
+        longitude: 100.5685,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers:[
+      {
+        latlng:{
+          latitude:13.9,
+          longitude:100.8
+        },
+        title:'Station',      
+        description:'position of station',
+      }
+      ]
+    };
+  }
+
+  componentWillMount() {
+      axios.get('http://localhost:9090/api/')
+        .then(response => this.setState({
+
+        }));
+    }
+
+  onRegionChange(region) {
+    console.log(region)
+    this.setState({
+      region,
+    });
+  }
+
+  onPress(cor,pos) {
+    console.log(cor,pos)
+    this.setState({
+      cor,pos,
+    });
+  }
+
+
   render() {
+    console.log(this.state.markers[0])
     return (
-      
-       <MapView style = {styles.container}
-       followsUserLocation = {true}
-       showsUserLocation = {true}
-    initialRegion={{
-      latitude: 37.78825,
-      longitude: -100.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    }} />
-    
+      <View style={{flex:1}}>
+      <MapView style={{flex:1}}
+        region={this.state.region}
+        showsUserLocation = {true}
+        showsMyLocationButton = {true}
+        loadingEnabled = {true}
+        onRegionChange={ (x)=>this.onRegionChange(x)}
+        onPress = { (a)=>console.log(a) }
+      >
+        {this.state.markers.map(marker => (
+        <MapView.Marker
+            coordinate={marker.latlng}
+            title={marker.title}
+            description={marker.description}
+            />
+          ))}  
+        </MapView>
+        </View>
     );
   }
+
+//   render() {
+//     return (
+//     <View style={{flex:1}}>
+
+//        <MapView style = {{flex:1,margin:10}}
+//         userLocationAnnotationTitle = {this.state.annotate}
+//         region={
+//           this.state.region
+//         }
+//         onRegionChange = { (x)=> this.changeLocation(x) }/>
+//      <View style={{flex:1}}>
+      
+//      </View>
+// </View>
+//     );
+//   }
 }
 
 const styles = StyleSheet.create({
